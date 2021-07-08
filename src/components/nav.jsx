f70ae39cadca4bd5940d63101d1f81831d203f31
MadhushaPrasad <madushaprasad21@gmail.com>
2021-07-08 16:13:07 +0530

    change some styles of nav component and theme.js

--- src/components/nav.jsx
+++ src/components/nav.jsx
@@ -4,12 +4,22 @@ import { jsx } from 'theme-ui';
 import Link from 'next/link';
 
 const Nav = () => {
-  <header sx={{ height: '60px', width: '100vw', bg: 'primary' }}>
+  <header
+    sx={{
+      height: '60px',
+      width: '100vw',
+      bg: 'primary',
+      borderBottom: '1px solid',
+      borderColor: 'primary',
+    }}
+  >
     <nav
       sx={{
         display: 'flex',
         alignItem: 'center',
         justifyContent: 'space-between',
+        variant: 'containers.page',
+        height: '100%',
       }}
     >
       <Link href="/">
--- theme.js
+++ theme.js
@@ -2,6 +2,24 @@ import { roboto } from '@theme-ui/presets';
 
 const theme = {
   ...roboto,
+  containers: {
+    card: {
+      shadow: '0 1px 3px rgba(0,0,0,0.12),0 1px 2px rgba(0,0,0,0)',
+      border: '1px solid',
+      borderColor: 'muted',
+      borderRadius: '4px',
+      p: 4,
+    },
+    page: {
+      width: '100%',
+      maxWidth: '960px',
+      m: 0,
+      mx: 'auto',
+    },
+  },
+  styles: {
+    ...roboto.styles,
+  },
   fonts: {
     body: 'system-ui, sans-serif',
     heading: '"Avenir Next", sans-serif',

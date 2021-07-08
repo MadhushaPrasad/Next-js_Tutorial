617f3f8fed7eba77a0c42bd8a0265fe568058413
MadhushaPrasad <madushaprasad21@gmail.com>
2021-07-08 16:46:29 +0530

    import nav componet to _app.jsx

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,11 +1,13 @@
 import '../src/styles.module.css';
 import { ThemeProvider } from 'theme-ui';
+import Nav from '../src/components/nav';
 
 import theme from '../theme';
 
 export default function App({ Component, pageProps }) {
   return (
     <ThemeProvider theme={theme}>
+      <Nav />
       <Component {...pageProps} />
     </ThemeProvider>
   );
--- src/components/nav.jsx
+++ src/components/nav.jsx
@@ -1,37 +1,40 @@
+/** @jsxRuntime classic */
 /** @jsx jsx */
 
 import { jsx } from 'theme-ui';
 import Link from 'next/link';
 
 const Nav = () => {
-  <header
-    sx={{
-      height: '60px',
-      width: '100vw',
-      bg: 'primary',
-      borderBottom: '1px solid',
-      borderColor: 'primary',
-    }}
-  >
-    <nav
+  return (
+    <header
       sx={{
-        display: 'flex',
-        alignItem: 'center',
-        justifyContent: 'space-between',
-        variant: 'containers.page',
-        height: '100%',
+        height: '60px',
+        width: '100vw',
+        bg: 'primary',
+        borderBottom: '1px solid',
+        borderColor: 'primary',
       }}
     >
-      <Link href="/">
-        <a sx={{ fontWight: 'bold', fontSize: 4, cursor: 'pointer' }}>
-          Note App
-        </a>
-      </Link>
-      <Link href="/notes">
-        <a sx={{ color: 'text', fontSize: 3, cursor: 'pointer' }}>Notes</a>
-      </Link>
-    </nav>
-  </header>;
+      <nav
+        sx={{
+          display: 'flex',
+          alignItem: 'center',
+          justifyContent: 'space-between',
+          variant: 'containers.page',
+          height: '100%',
+        }}
+      >
+        <Link href="/">
+          <a sx={{ fontWight: 'bold', fontSize: 4, cursor: 'pointer' }}>
+            Note App
+          </a>
+        </Link>
+        <Link href="/notes">
+          <a sx={{ color: 'text', fontSize: 3, cursor: 'pointer' }}>Notes</a>
+        </Link>
+      </nav>
+    </header>
+  );
 };
 
 export default Nav;
